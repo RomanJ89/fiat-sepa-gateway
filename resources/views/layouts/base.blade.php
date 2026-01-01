3586356a389eddc911c1be4dc584ef35daf9e9a2
RomanJ89 <36987586+RomanJ89@users.noreply.github.com>
2026-01-01 00:00:54 -0500

    UI: Update global layout navigation to match gateway services

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>@yield('title')</title>
-  <link rel="stylesheet" href="{{ URL::asset('css/base.css') }}">
+    <meta charset="utf-8">
+    <title>Base58 Gateway - @yield('title')</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/base.css') }}">
 </head>
 
-  <body>
+<body>
     <header>
-      <h1>Bibliotheque</h1>
+        <h1>Fiat-SEPA Gateway Control</h1>
 
-      <div class="link">
-        <a href="/">Accueil</a>
-        <a href="/insert">Inserer un livre</a>
-        <a href="/insertGenre">Ajouter un genre</a>
-      </div>
+        <div class="link">
+            <a href="/">Dashboard</a>
+            <a href="/insert">New Payment</a>
+            <a href="/insertGenre">Manage Nodes</a>
+        </div>
     </header>
+    
     <main>
-      @yield("main")
+        @yield("main")
     </main>
-
-  </body>
+</body>
 </html>
