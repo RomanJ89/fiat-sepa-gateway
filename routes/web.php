9db62f7232fde85fc6f257ca9cfae85dc2b4ad10
RomanJ89 <36987586+RomanJ89@users.noreply.github.com>
2025-12-31 23:49:24 -0500

    Feature: Implement secure routes for settlement processing

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,17 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes - Base58 Fiat Gateway
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
+Route::get('/', function () {
+    return response()->json(['service' => 'Base58 SEPA Gateway', 'status' => 'operational']);
+});
 
-Route::get ('/', 'LibraryController@index');
-Route::get ('/insert', 'LibraryController@insert');
-Route::get ('/update', 'LibraryController@update');
-Route::get ('/insertGenre', 'LibraryController@insertGenre');
-
-Route::post ('/insert', 'InsertController@insertBook');
-Route::post ('/delete', 'InsertController@deleteBook');
-Route::post ('/update', 'InsertController@updateBook');
-Route::post ('/insertGenre', 'InsertController@insertGenre');
+// Internal Settlement & Reporting Routes
+Route::group(['middleware' => 'auth', 'prefix' => 'settlement'], function () {
+    Route::get('/batches', 'SettlementController@index');
+    Route::post('/process', 'SettlementController@processBatch');
+    Route::get('/report/{id}', 'SettlementController@generateReport');
+});
